94b57200a11d07523a7d86c26b3bf4cc678f31ef
Michel Grootjans <michel.grootjans@gmail.com>
2018-03-01 12:02:43 +0100

    introduced the rubber duck

--- src/main/java/headfirst/ducks/DecoyDuck.java
+++ src/main/java/headfirst/ducks/DecoyDuck.java
@@ -1,14 +1,14 @@
 package headfirst.ducks;
 
-public class RubberDuck extends Duck {
+public class DecoyDuck extends Duck {
     @Override
     public String display() {
-        return "I'm a rubber duck";
+        return "I'm a decoy duck";
     }
 
     @Override
     public String quack() {
-        return "Squeak!";
+        return "-- mute --";
     }
 
     @Override
