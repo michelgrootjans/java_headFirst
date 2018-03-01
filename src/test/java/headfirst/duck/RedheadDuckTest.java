f59653f0e09547b84074cbe78f2a8d4750962014
Michel Grootjans <michel.grootjans@gmail.com>
2018-03-01 10:39:41 +0100

    introduced the redhead duck

---
+++ src/test/java/headfirst/duck/RedheadDuckTest.java
@@ -0,0 +1,21 @@
+package headfirst.duck;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RedheadDuckTest {
+    @Test
+    public void quack() {
+        assertThat(new RedheadDuck().quack(), is("Quack!"));
+    }
+    @Test
+    public void swim() {
+        assertThat(new RedheadDuck().swim(), is("Splish splash ..."));
+    }
+    @Test
+    public void display() {
+        assertThat(new RedheadDuck().display(), is("I'm a mallard duck"));
+    }
+}
